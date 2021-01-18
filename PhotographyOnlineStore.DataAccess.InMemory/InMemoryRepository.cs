8e229b6744cedba7bdb4f3bef9a9269cff0dbf3b
sobhi@cloudulu.com <sobhi@cloudulu.com>
2021-01-18 13:07:18 +0100

    Adding In memory repo

---
+++ PhotographyOnlineStore.DataAccess.InMemory/InMemoryRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhotographyOnlineStore.DataAccess.InMemory
+{
+    public class InMemoryRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<T> items;
+        string className;
+
+        public InMemoryRepository()
+        {
+            className = typeof(T).Name;
+            items = cache[className] as List<T>;
+            if (items == null)
+            {
+                items = new List<T>();
+            }
+        }
+        public void commit()
+        {
+            cache[className] = items;
+        }
+        public void insert(T t)
+        {
+            items.Add(t);
+        }
+        public void Update(T t)
+        {
+            T tToupdate = items.Find(i => i.Id == t.Id);
+            if (tToupdate != null)
+            {
+                tToupdate = t;
+            }
+            else
+            {
+                throw new Exception(className + "Not Found");
+            }
+        }
+        public T Find(string Id)
+        {
+            T t = items.Find(i => i.Id == Id);
+            if (t != null)
+            {
+                return t;
+            }
+            else
+            {
+                throw new Exception(className + "Not Found");
+            }
+
+        }
+        public IQueryable<T> Collection()
+        {
+            return items.AsQueryable();
+        }
+        public void Delete(string Id)
+        {
+            T tDelete = items.Find(i => i.Id == Id);
+            if (tDelete != null)
+            {
+                items.Remove(tDelete);
+            }
+            else
+            {
+                throw new Exception(className + "Not Found");
+            }
+        }
+    }
+}
--- PhotoographyOnlineStore/ViewModels/ProductListViewModel.cs
+++ PhotoographyOnlineStore/ViewModels/ProductListViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using.PhotoographyOnlineStore.Core.Models;
 
 namespace PhotoographyOnlineStore.ViewModels
 {
